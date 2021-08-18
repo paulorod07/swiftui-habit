eecb5af00875eeebbdade349950f8569523cb3fa
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-08-17 23:10:23 -0300

    init project

---
+++ Habit/Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Paulo Rodrigues on 04/03/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
