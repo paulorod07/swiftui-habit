3c6ef3ad33b660bbd53e216f32882b043e273366
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-08-18 12:07:04 -0300

    fazendo tela de login

--- Habit/Habit/SignIn/View/SignInView.swift
+++ Habit/Habit/SignIn/View/SignInView.swift
@@ -11,8 +11,99 @@ struct SignInView: View {
     
     @ObservedObject var viewModel: SignInViewModel
     
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var navigationHidden: Bool = true
+    
+    @State var action: Int? = 0
+    
     var body: some View {
-        Text("Olá signIn")
-            .background(Color.blue)
+        NavigationView {
+            
+            ScrollView(showsIndicators: false) {
+                
+                VStack(alignment: .center, spacing: 20) {
+                    
+                    Spacer(minLength: 36)
+                    
+                    VStack(alignment: .center, spacing: 8) {
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.horizontal, 48)
+                        
+                        Text("Login")
+                            .foregroundColor(.orange)
+                            .font(Font.system(.title).bold())
+                            .padding(.bottom, 8)
+                        
+                        numberField
+                        
+                        passwordField
+                        
+                        enterButton
+                        
+                        registerLink
+                        
+                        Text("Copyright @Paulo Rodrigues")
+                            .foregroundColor(Color.gray)
+                            .font(Font.system(size: 16).bold())
+                            .padding(.top, 16)
+                    }
+                    
+                }
+                
+            }
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+            .padding(.horizontal, 32)
+            .background(Color.white)
+            .navigationBarTitle("Login", displayMode: .inline)
+            .navigationBarHidden(navigationHidden)
+        }
+    }
+}
+
+extension SignInView {
+    var numberField: some View {
+        TextField("", text: $email)
+            .border(Color.black)
+    }
+    
+    var passwordField: some View {
+        SecureField("", text: $password)
+            .border(Color.black)
+    }
+    
+    var enterButton: some View {
+        Button("Entrar") {
+            print("Apertou em entrar")
+        }
+    }
+    
+    var registerLink: some View {
+        VStack {
+            Text("Ainda não possui login?")
+                .foregroundColor(.gray)
+                .padding(.top, 48)
+            
+            ZStack {
+                NavigationLink(
+                    destination: Text("Tela de cadastro"),
+                    tag: 1,
+                    selection: $action,
+                    label: { EmptyView() })
+                
+                Button("Realize seu Cadastro") {
+                    self.action = 1
+                }
+            }
+        }
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel: SignInViewModel = SignInViewModel()
+        SignInView(viewModel: viewModel)
     }
 }
